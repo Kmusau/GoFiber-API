0cec1513897611a8fea805174729966b5a9114a8
kevo <kevomusa6565@gmail.com>
2022-03-09 08:14:07 +0300

    First API

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Get("/", hello)
+
+	log.Fatal(app.Listen(":3000"))
+}
+
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Welcome to Go API")
+}
